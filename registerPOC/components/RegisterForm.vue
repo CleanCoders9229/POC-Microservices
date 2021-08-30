72af5ba4d520c622cf559cc4c4cbfbe0b5aa0d83
adadesions <adadesions@gmail.com>
2021-08-30 18:12:53 +0700

    Init Services

---
+++ registerPOC/components/RegisterForm.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <form>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label"
+            >Email address</label
+          >
+          <input
+            type="email"
+            class="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+          <div id="emailHelp" class="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Fullname</label>
+          <input type="text" class="form-control" id="fullname" />
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">Password</label>
+          <input
+            type="password"
+            class="form-control"
+            id="exampleInputPassword1"
+          />
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label"
+            >Confirm Password</label
+          >
+          <input
+            type="password"
+            class="form-control"
+            id="exampleInputPassword1"
+          />
+        </div>
+        <div class="d-grid gap-2">
+          <button type="button" class="btn btn-primary">Submit</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RegisterForm",
+};
+</script>
+
+<style>
+</style>
---
+++ registerPOC/pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="index-container">
+    <navbar></navbar>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg col-sm-12">
+          <login-form></login-form>
+        </div>
+        <div class="col-lg col-md-12">
+          <register-form></register-form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import LoginForm from "../components/LoginForm.vue";
+import Navbar from "../components/Navbar.vue";
+import RegisterForm from "../components/RegisterForm.vue"
+export default {
+  components: {
+    LoginForm,
+    Navbar,
+    RegisterForm
+  },
+};
+</script>
